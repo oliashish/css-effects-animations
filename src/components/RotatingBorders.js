3bdfee0b4ad98466d9f383109aa087afd15c1c65
Ashish oli <oliashish3@gmail.com>
2021-05-16 20:14:17 +0530

    complete: border framed on hover effect

--- src/components/RotatingBorders.js
+++ src/components/RotatingBorders.js
@@ -4,7 +4,11 @@ import "../styles/RotatingBorder.css";
 const RotatingBorders = () => {
     return (
         <div className="rotating-border-effect-container">
-            <div className="border-box"></div>
+            <div className="rotating-border-box"></div>
+            <div className="border-animation"></div>
+            <div className="frame-on-hover-border-box">
+                <h3>Hover Over Me :-)</h3>
+            </div>
         </div>
     );
 };
